b5392d2baa9ce224e1b791155f49397d18e49e6e
JONATAS JACKSON GONÇALVES <jonatas_goncalves@estudante.sesisenai.org.br>
2023-04-20 21:53:17 -0300

    Criação do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './Calculadora/calculadora.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CalculadoraModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
