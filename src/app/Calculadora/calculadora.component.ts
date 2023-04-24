479fd2ec4053d316d0659932aedb0619fc4bd2de
JONATAS JACKSON GONÇALVES <jonatas_goncalves@estudante.sesisenai.org.br>
2023-04-24 16:21:20 -0300

    Concerto de alguns problemas com o '.'

--- src/app/Calculadora/calculadora.component.ts
+++ src/app/Calculadora/calculadora.component.ts
@@ -7,57 +7,68 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class CalculadoraComponent {
 
 
-
-  textoB: string = ""
-  resultado = true;
-  ponto = false;
+  igual:boolean = false;
+  textoB: string = "";
+  ponto:boolean = true;
 
   clicou(texto: string): void {
     if (texto == "Delete") {
-      this.textoB = ""
-      this.ponto = false;
-    }
-    else if (texto == "Back") {
-      if (this.textoB.slice(-1) == ".") {
-        this.ponto = false;
-      }
-      this.textoB = this.textoB.substring(0, this.textoB.length - 1)
-    }
-    else if (texto == "=") {
-      this.textoB = eval(this.textoB)
-      this.resultado = false;
-      this.ponto = false;
-    } else if (texto == "." && this.ponto == false) {
-      this.textoB += "."
+      this.textoB = "";
+      this.igual = false;
       this.ponto = true;
     }
-    else {
-
-      if (this.textoB.slice(-1) == "+" || this.textoB.slice(-1) == "-" || this.textoB.slice(-1) == "*" || this.textoB.slice(-1) == "/") {
+    else if (texto == "Back") {
+      if(this.textoB.slice(-1) == "."){
+        this.ponto = false;
+      }
+      this.textoB = this.textoB.substring(0, this.textoB.length - 1);
+      this.igual = false;
+    }
+    else if (texto == "=") {
+      if(this.textoB.slice(-1) == "+" || this.textoB.slice(-1) == "/" || this.textoB.slice(-1) == "*" || this.textoB.slice(-1) == "-"){
+        this.textoB = this.textoB.substring(0, this.textoB.length - 1);
+      }
+      this.textoB = eval(this.textoB).toString()
+      this.igual = true;
+    } 
+    else if (texto == ".") {
+      if(this.textoB.slice(-1) == "+" || this.textoB.slice(-1) == "/" || this.textoB.slice(-1) == "*" || this.textoB.slice(-1) == "-"){
         this.ponto = true;
-        if (texto != "+" && texto != "-" && texto != "*" && texto != "/" && texto != ".") {
-          this.ponto = false;
+      }
+      if(!this.ponto){
+        this.textoB += "."
+        this.ponto = true;
+      }
+      if(this.igual){
+        this.igual = false;
+        this.ponto = true;
+        this.textoB = "";
+      }
+    }
+    else {
+      if(texto == "+" || texto == "/" || texto == "*" || texto == "-"){
+        this.igual = false;
+        if(this.textoB.slice(-1) != "+" && this.textoB.slice(-1) != "/" && this.textoB.slice(-1) != "*" && this.textoB.slice(-1) != "-"){
+          this.textoB+=texto;
+        } else{
+          this.textoB = this.textoB.substring(0, this.textoB.length - 1)
+          this.textoB+=texto;
         }
       }
+      else{
 
-      if (texto != ".") {
-        if (!this.resultado && texto != "+" && texto != "-" && texto != "*" && texto != "/") {
+        if(this.textoB.slice(-1) == "+" || this.textoB.slice(-1) == "/" || this.textoB.slice(-1) == "*" || this.textoB.slice(-1) == "-"){
+          this.ponto = false;
+        }
 
-          this.textoB = ""
-          this.resultado = true;
+        if(this.textoB.length==0){
+          this.ponto = false;
         }
-        else {
-          this.resultado = true;
+        if(this.igual == true){
+          this.textoB = "";
+          this.igual = false;
         }
-        if (texto == "+" || texto == "-" || texto == "*" || texto == "/" || texto == ".") {
-
-          if (this.textoB.slice(-1) == "+" || this.textoB.slice(-1) == "-" || this.textoB.slice(-1) == "*" || this.textoB.slice(-1) == "/" || this.textoB.slice(-1) == ".") {
-          } else {
-            this.textoB += texto;
-          }
-        } else {
-          this.textoB += texto;
-        }
+        this.textoB+=texto;
       }
     }
   }
